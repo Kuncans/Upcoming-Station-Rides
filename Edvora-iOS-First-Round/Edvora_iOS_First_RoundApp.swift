d90e83296262b64a10bab77779742d12cd7f963a
Kuncans <akafauna@gmail.com>
2022-02-22 13:07:21 +0000

    Initial Commit

---
+++ Edvora-iOS-First-Round/Edvora_iOS_First_RoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edvora_iOS_First_RoundApp.swift
+//  Edvora-iOS-First-Round
+//
+//  Created by Duncan Kent on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Edvora_iOS_First_RoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
